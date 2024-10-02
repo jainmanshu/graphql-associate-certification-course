b593da9573ddb595b466a54be148e8e4c9ac7b2d
Himanshu Jain <jainmanshu@gmail.com>
2024-10-02 13:51:57 -0500

    adding projects

---
+++ odyssey-lift-off-part1/client/src/index.tsx
@@ -0,0 +1,20 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Pages from "./pages";
+import GlobalStyles from "./styles";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
+  </React.StrictMode>
+);
